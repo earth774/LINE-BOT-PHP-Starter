a96b1dde680b22815646c9906f82bca92f6e24b2
เอิร์ท <sutthiphongnuanma@gmail.com>
2017-01-21 21:31:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -29,6 +29,7 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
+			echo $post;
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
 			$ch = curl_init($url);
