3799337515415cd7f202215a32c408dbfa029a8d
เอิร์ท <sutthiphongnuanma@gmail.com>
2017-01-21 16:00:11 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,9 @@
+<html>
+  <head><title>TestInput</title></head>
+  <body>
+    <form action="bot.php">
+      <input type="text" name="message" value="">
+      <input type="submit" value="Submit">
+    </form>
+  </body>
+</html>
