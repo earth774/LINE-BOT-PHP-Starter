6befcad4e10968439b3eba2bee40dd2730578ca7
เอิร์ท <sutthiphongnuanma@gmail.com>
2017-01-22 15:46:35 +0700

    Create temp_file.php

---
+++ temp_file.php
@@ -0,0 +1,31 @@
+<?php
+	function avg($url){
+					$url = "http://api.openweathermap.org/data/2.5/forecast/weather?q=phayao,th&APPID=a66d4a763bdf2b109ee7c0b91796a3c9";
+			// Get POST body content
+			$content = file_get_contents($url);
+			// Parse JSON
+			$obj = json_decode($content, true);
+
+			// echo $obj['city']['name']." , ";
+			// echo $obj['city']['country'];
+			// echo count($obj['list']);
+			for ($i=0; $i <count($obj['list']) ; $i++) { 
+
+				if($data['max']<$obj['list'][$i]['main']['temp_max']){ // find max
+					$data['max']=$obj['list'][$i]['main']['temp_max'];
+				}
+				if($data['min']>$obj['list'][$i]['main']['temp_min']){ // find min
+					$data['min']=$obj['list'][$i]['main']['temp_min'];
+				}
+				$data['mid']+=$obj['list'][$i]['main']['temp_max'];
+			}
+				$data['mid']=$data['mid']/count($obj['list']);
+
+
+			$str = "เมือง ".$obj['city']['name'].
+			" อุณหภูมิสูงสุด (temperature_max) : ".round(($data['max']-273.15),2).
+			"℃ อุณหภูมิต่ำสุด (temperature_min) : ".round(($data['min']-273.15),2).
+			"℃ อุณหภูมิเฉลี่ย (temperature_avg) : ".round(($data['mid']-273.15),2)."℃";
+			return $str;
+	}		
+?>
