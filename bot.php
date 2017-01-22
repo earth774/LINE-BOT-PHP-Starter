3721c0e9d0b6284b526382375c8709095b4ed35b
เอิร์ท <sutthiphongnuanma@gmail.com>
2017-01-22 15:46:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,6 @@
 <?php
+require_once("temp_file.php");
+
 $access_token = 'tE5TtVey741f7fDa2y1u9+NYbfhdbqRItsIE2vnbPW2uWusExRsQwnuuRX1+CVvtPCP+vwnPPgZ3wKrmIYKGHRn4QcqAlySw1879KZOHLfoVvid/E5BNsT+f0ptGwzMGvBcA3CHjyv3hoxK0TXyQAAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -22,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $link
+				'text' => avg($link)
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
