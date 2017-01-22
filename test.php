9dd62e930f32b81cfd66c6abedff7476886da982
เอิร์ท <sutthiphongnuanma@gmail.com>
2017-01-22 15:46:51 +0700

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <head><title>TestInput</title></head>
-  <body>
-    <form action="bot.php" method="POST">
-      <input type="text" name="message" value="">
-      <input type="submit" value="Submit">
-    </form>
-  </body>
-</html>
