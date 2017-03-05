93027b96e5c0e572b80d4e91865b2002b01ef8e4
เอิร์ท <sutthiphongnuanma@gmail.com>
2017-03-05 16:35:02 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -34,18 +34,14 @@ if (!is_null($events['events'])) {
 			    "previewImageUrl"=>"https://raw.githubusercontent.com/kittinan/Sample-Line-Bot/master/images/beer_preview.jpg"
 			];
 			
-			$stk = [
-				"type"=>"sticker",
-				"packageId"=>"1",
-				"stickerId"=>1
-			];
+			
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$mg,$messages,$stk],
+				'messages' => [$mg,$messages],
 			];
 			
 			$post = json_encode($data);
